9dfd4675f4120cdc42612a5d15f84674ef78ab41
Liuwu <Liuwu@qq.com>
2024-07-28 04:15:28 +0800

    更新urls对应视图函数

--- kk_blog/blog/views.py
+++ kk_blog/blog/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # 首页的视图函数
 def index(request):
     return render(request, "index.html")
+
+
+# 博客详情视图函数
+def blog_detail(request, blog_id):
+    return render(request, "blog_detail.html")
+
+
+def pub_blog(request):
+    return render(request, "pub_blog.html")
