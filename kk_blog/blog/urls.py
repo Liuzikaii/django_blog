39a4b6f20fe0d5d4b4b168810e633403274d986d
Liuwu <Liuwu@qq.com>
2024-07-28 04:13:51 +0800

    添加博客详情、发布博客页面

--- kk_blog/blog/urls.py
+++ kk_blog/blog/urls.py
@@ -5,4 +5,6 @@ app_name = 'blog'  # 指定 app 命名空间
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('blog/<int:blog_id>', views.blog_detail, name='blog_detail'),
+    path('blog/pub', views.pub_blog, name='pub_blog'),
 ]
