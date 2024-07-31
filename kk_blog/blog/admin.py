e4fdd420adba96576e7cd77174a0dcdd9d3be531
Liuwu <Liuwu@qq.com>
2024-08-01 01:31:23 +0800

    新增博客、博客分类、博客评论的Django自带后台管理系统增删改查功能

--- kk_blog/blog/admin.py
+++ kk_blog/blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import BlogCategory, Blog, BlogComment
+
 
 # Register your models here.
+class BlogCategoryAdmin(admin.ModelAdmin):
+    # 这个模型中哪些字段是可以被编辑的
+    list_display = ['name']
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'publish_time', 'category', 'author')
+
+
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'publish_time', 'blog', 'author')
+
+
+# 管理BlogCategory这个模型的时候 用的是BlogCategoryAdmin这个Admin
+admin.site.register(BlogCategory, BlogCategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(BlogComment, BlogCommentAdmin)
+
+
